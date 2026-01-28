400d8235d47c4df4ea6bd794de2e6780a459b100
Andrey Demenev <andrey@1crm.com>
2026-01-28 23:12:49 +0400

    version 0.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Plumbery.MixProject do
     [
       app: :plumbery,
       description: description(),
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       consolidate_protocols: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
